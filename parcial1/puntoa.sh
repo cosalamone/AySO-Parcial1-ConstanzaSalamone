a7344126e2691d54276841d7070b82d925291888
cosalamone <cosalamone@gmail.com>
2024-10-17 23:27:06 +0000

    fix path script puntoA

--- parcial1/puntoa.sh
+++ parcial1/puntoa.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-mkdir -p /home/vagrant/parcial1/Examenes-UTN/{alumno_{1,2,3}/parcial_{1..3},profesores}
+mkdir -p /home/vagrant/AySO-Parcial1-ConstanzaSalamone/parcial1/Examenes-UTN/{alumno_{1,2,3}/parcial_{1..3},profesores}
+
+echo "El script del punto A se ejecutó y las carpetas fueron creadas."
 
